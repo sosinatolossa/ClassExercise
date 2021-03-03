7896c3f8c5d6c0ab07698164b2ab2588394ec665
sosinatolossa <sosinatolossa16@gmail.com>
2021-03-03 15:57:44 -0600

    made changes

--- Program.cs
+++ Program.cs
@@ -1,95 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ClassExercise
+﻿class Program
 {
-    public class Customer
+    static void Main(string[] args)
     {
-        // Public Properties
-        public string FirstName { get; set; }
+        // Create an instance of a company. Name it whatever you like.
 
-        public string LastName { get; set; }
+        // Create three employees
 
-        public bool IsLocal { get; set; }
-
-        public string FullName
-        {
-            get
-            {
-                return $"{FirstName} {LastName}";
-            }
-        }
-    }
-
-    public class DeliveryService
-    {
-        /*
-          Properties
-        */
-        public string Name { get; set; }
-
-        public string TransitType { get; set; }
+        // Assign the employees to the company
 
         /*
-          Methods
+            Iterate the company's employee list and generate the
+            simple report shown above
         */
-        public void Deliver(Product product, Customer customer)
-        {
-            Console.WriteLine($"Product delivered by {TransitType} to {customer.FullName}");
-        }
-    }
-
-    public class Product
-    {
-        /*
-          Properties
-        */
-        public string Title { get; set; }
-
-        public string Description { get; set; }
-
-        public double Price { get; set; }
-
-        public int Quantity { get; set; }
-
-        /*
-          Methods
-        */
-        public void Ship(Customer customer, DeliveryService service)
-        {
-            if (!customer.IsLocal)
-            {
-                service.Deliver(this, customer);
-            }
-        }
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Product tinkerToys = new Product()
-            {
-                Title = "Tinker Toys",
-                Description = "You can build anything you want",
-                Price = 32.49,
-                Quantity = 25
-            };
-
-            Customer marcus = new Customer()
-            {
-                FirstName = "Marcus",
-                LastName = "Fulbright",
-                IsLocal = false
-            };
-
-            DeliveryService UPS = new DeliveryService()
-            {
-                Name = "UPS",
-                TransitType = "train"
-            };
-
-            // Ship the tinker toys to Marcus using UPS
-            tinkerToys.Ship(marcus, UPS);
-        }
     }
 }
